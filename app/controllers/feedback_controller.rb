b8cc5d4966fb7e3e1f8046203b5d5f972f29fccf
Robbie Clutton <robert.clutton@gmail.com>
2013-03-21 23:04:19 -0400

    Trying out different routing

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -1,2 +1,27 @@
 class FeedbackController < ApplicationController
+
+  before_filter do
+    @feedback_group = FeedbackGroup.find_by_slug(params[:group_id])
+  end
+
+  def group
+    if @feedback_group
+      render 'feedback_groups/show'
+    else
+      render 'feedback_groups/new'
+    end
+  end
+
+  def case
+    if @feedback_group
+      @feedback_case = @feedback_group.feedback_cases.where(slug: params[:case_id]).first
+      if @feedback_case
+        render 'feedback_cases/show'
+      else
+        render 'feedback_cases/new'
+      end
+    else
+      redirect_to feedback_groups_show_path(params[:group_id])
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,17 @@ Feedback::Application.routes.draw do
   resources :feedback_cases, only: [:create]
   resources :feedback_scores, only: [:create]
 
-  scope ":id" do
-    get '', to: "feedback_groups#show", as: "feedback_groups_show"
+  scope ":group_id" do
+    get '', to: "feedback#group"
+    get ':case_id', to: "feedback#case"
   end
 
-  scope ":feedback_group_id" do
-    get ':id', to: "feedback_cases#show", as: "feedback_cases_show"
-  end
+  # scope ":id" do
+    # get '', to: "feedback_groups#show", as: "feedback_groups_show"
+  # end
+
+  # scope ":feedback_group_id" do
+    # get ':id', to: "feedback_cases#show", as: "feedback_cases_show"
+  # end
 
 end
